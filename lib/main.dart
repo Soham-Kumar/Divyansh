d88caa0862fe0b201380bf938b3037b0905fa587
Soham Kumar <sohamk0278@gmail.com>
2022-12-06 23:43:57 +0530

    Bruh

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,41 @@ class MyApp extends StatefulWidget
   const MyApp({Key? key}) : super(key: key);
   @override
   State<MyApp> createState() => _MyAppState();
+
 }
 
 class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
 
-  int _currentIndex = 0;
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: const Text("Orders", style: TextStyle(color: Colors.white, fontSize: 30)),
+            title: const Text(
+                "Orders", style: TextStyle(color: Colors.white, fontSize: 30)),
             titleSpacing: 00.0,
             centerTitle: true,
             toolbarHeight: 70,
@@ -33,87 +57,108 @@ class _MyAppState extends State<MyApp> {
             elevation: 0.00,
             backgroundColor: Colors.blueAccent[400],
           ),
-          body: GridView.builder(
-            itemCount: 5,
-            gridDelegate:
-                const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-            itemBuilder: (context, index) {
-              return Card(
-                  color: Colors.blueAccent,
-                  shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-              ),
-                child: Column(
-                  children: [
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(context,
-                      MaterialPageRoute(builder: (context) {
-                      return const SecondPage(title: 'SecondPage');
-                          }
-                        )
-                      );
-                    },
-                    child: Container(
-                    padding: const EdgeInsets.fromLTRB(30.0,20.0,30.0,0.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.fromLTRB(0.0, 8.0, 8.0, 8.0),
-                          child: Text(
-                            'Text',
-                            textAlign: TextAlign.left,
-                            style: TextStyle(color: Colors.brown, fontSize: 20),
-                          ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.fromLTRB(0.0, 5.0, 8.0, 8.0),
-                          child: Text(
-                            'Answer',
-                            textAlign: TextAlign.left,
-                            style: TextStyle(color: Colors.brown, fontSize: 20),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(0.0, 5.0, 8.0, 8.0),
-                          child: IconButton(
-                            icon: const Icon(Icons.delete, color: Colors.brown, size: 40.0),
-                            tooltip: 'Delete',
-                            onPressed: () {
-                            },
-                          ),
-                        ),
-                      ],
-                    )
-                ),
-              ),
 
-                  ],
-                ),
-              );
-            },
+          body: Column(
+            children: [
+              Divider(),
+              GridView.builder(
+                itemCount: 5,
+                gridDelegate:
+                const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: Colors.blueAccent,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                    child: Column(
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(context,
+                                MaterialPageRoute(builder: (context) {
+                                  return const SecondPage(title: 'SecondPage');
+                                }
+                                )
+                            );
+                          },
+                          child: Container(
+                              padding: const EdgeInsets.fromLTRB(
+                                  30.0, 20.0, 30.0, 0.0),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  const Padding(
+                                    padding: EdgeInsets.fromLTRB(
+                                        0.0, 8.0, 8.0, 8.0),
+                                    child: Text(
+                                      'Text',
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                          color: Colors.brown, fontSize: 20),
+                                    ),
+                                  ),
+                                  const Padding(
+                                    padding: EdgeInsets.fromLTRB(
+                                        0.0, 5.0, 8.0, 8.0),
+                                    child: Text(
+                                      'Answer',
+                                      textAlign: TextAlign.left,
+                                      style: TextStyle(
+                                          color: Colors.brown, fontSize: 20),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        0.0, 5.0, 8.0, 8.0),
+                                    child: IconButton(
+                                      icon: const Icon(
+                                          Icons.delete, color: Colors.brown,
+                                          size: 40.0),
+                                      tooltip: 'Delete',
+                                      onPressed: () {},
+                                    ),
+                                  ),
+                                ],
+                              )
+                          ),
+                        ),
+
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
           bottomNavigationBar: BottomNavigationBar(
-            currentIndex: _currentIndex,
-            type: BottomNavigationBarType.fixed,
-            items: [
+            items: const <BottomNavigationBarItem>[
               BottomNavigationBarItem(
-                  icon: Icon(Icons.home),
-//                  title: Text('Home'),
-              backgroundColor: Colors.blue),
+                icon: Icon(Icons.home),
+                label: 'Home',
+                backgroundColor: Colors.red,
+              ),
               BottomNavigationBarItem(
-                  icon: Icon(Icons.search),
-//                  title: Text('Home'),
-                  backgroundColor: Colors.blue),
+                icon: Icon(Icons.business),
+                label: 'Business',
+                backgroundColor: Colors.green,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.school),
+                label: 'School',
+                backgroundColor: Colors.purple,
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: 'Settings',
+                backgroundColor: Colors.pink,
+              ),
             ],
-            onTap: (index) {
-              setState(() {
-                _currentIndex = index;
-              });
-            }
+            currentIndex: _selectedIndex,
+            selectedItemColor: Colors.amber[800],
+            onTap: _onItemTapped,
           ),
-        ),
-    );
-  }
-}
+
+
+        ));
+  }}
